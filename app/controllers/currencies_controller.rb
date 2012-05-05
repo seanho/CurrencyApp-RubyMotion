9b88a54d026dd5c19d232eb7da32550d49df94ff
Sean Ho <sean@seanho.net>
2012-05-05 22:13:52 +1000

    filter table by currency code

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -5,6 +5,7 @@ class CurrenciesController < UIViewController
     if super
       self.title = "Currency"
       @infos = []
+      @search_infos = []
     end
     self
   end
@@ -17,17 +18,21 @@ class CurrenciesController < UIViewController
     @table_view.dataSource = self
     @table_view.delegate = self
     self.view.addSubview @table_view
+    
+    searchBar = UISearchBar.alloc.initWithFrame(CGRectZero)
+    searchBar.showsCancelButton = true;
+    searchBar.placeholder = "Enter Currency Code"
+    searchBar.keyboardType = UIKeyboardTypeAlphabet
+    searchBar.sizeToFit
+    @table_view.tableHeaderView = searchBar
+    
+    @search_controller = UISearchDisplayController.alloc.initWithSearchBar(searchBar, contentsController:self)
+    @search_controller.delegate = self
+    @search_controller.searchResultsDataSource = self
+    @search_controller.searchResultsDelegate = self
   end
   
   def viewDidLoad
-    loadData
-  end
-  
-  def viewDidUnload
-    @table_view = nil
-  end
-  
-  def loadData
     Dispatch::Queue.concurrent.async do
       update = CurrencyUpdate.latest
       
@@ -46,16 +51,44 @@ class CurrenciesController < UIViewController
     end
   end
   
+  def viewDidUnload
+    @table_view = nil
+  end
+  
+  # UISearchDisplayDelegate dataSource and delegate
+  def searchDisplayController(controller, shouldReloadTableForSearchScope:searchOption)
+    true
+  end
+  
+  def searchDisplayController(controller, shouldReloadTableForSearchString:searchOption)
+    search_text = controller.searchBar.text.upcase
+    if search_text.length > 0
+      @search_infos = @infos.select {|info| info.code.start_with?(search_text) }
+      @table_view.reloadData
+    end
+    true
+  end
+  
+  # UITableView dataSource and delegate
   def numberOfSectionsInTableView(tableView)
     1
   end
   
   def tableView(tableView, numberOfRowsInSection:section)
-    @infos.size
+    if (@search_controller && tableView == @search_controller.searchResultsTableView)
+      @search_infos.size
+    else
+      @infos.size
+    end
   end
 
   def tableView(tableView, cellForRowAtIndexPath:indexPath)
-    info = @infos[indexPath.row]
-    CurrencyInfoCell.cellForInfo(info, inTableView:tableView)
+    if (@search_controller && tableView == @search_controller.searchResultsTableView)
+      info = @search_infos[indexPath.row]
+      CurrencyInfoCell.cellForInfo(info, inTableView:tableView)
+    else
+      info = @infos[indexPath.row]
+      CurrencyInfoCell.cellForInfo(info, inTableView:tableView)
+    end
   end
 end
